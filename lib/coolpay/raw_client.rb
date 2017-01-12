626dd6c0fa306ab9ffb051a94ee0c3a1beaa7a90
Francesco Boffa <fra.boffa@gmail.com>
2017-01-12 14:45:04 +0100

    Added RawClient#get method

--- lib/coolpay/raw_client.rb
+++ lib/coolpay/raw_client.rb
@@ -7,10 +7,20 @@ module Coolpay
       @connection ||= Faraday.new(url: 'https://coolpay.herokuapp.com')
     end
 
-    def post(path, body = {})
+    def get(path, params = {}, token = nil)
+      response = connection.get do |req|
+        req.url "/api/#{path}", params
+        req.headers['Authorization'] = "Bearer #{token}" if token
+      end
+
+      parse_response(response)
+    end
+
+    def post(path, body = {}, token = nil)
       response = connection.post do |req|
         req.url "/api/#{path}"
         req.headers['Content-Type'] = 'application/json'
+        req.headers['Authorization'] = "Bearer #{token}" if token
         req.body = body.to_json
       end
 
--- spec/coolpay/raw_client_spec.rb
+++ spec/coolpay/raw_client_spec.rb
@@ -1,4 +1,36 @@
 describe Coolpay::RawClient do
+  describe '#get' do
+    it 'sends a GET request to the API server' do
+      request = stub_request(:get, 'https://coolpay.herokuapp.com/api/recipients')
+      subject.get('recipients')
+      expect(request).to have_been_requested
+    end
+
+    it 'sets the query string using the params' do
+      request = stub_request(:get, 'https://coolpay.herokuapp.com/api/recipients?name=francesco')
+      subject.get('recipients', { name: 'francesco' })
+      expect(request).to have_been_requested
+    end
+
+    it 'uses the optional token as an "Authorization Bearer" token' do
+      request = stub_request(:get, 'https://coolpay.herokuapp.com/api/recipients')
+                  .with(headers: { 'Authorization' => 'Bearer 123-456-789' })
+      subject.get('recipients', {}, '123-456-789')
+      expect(request).to have_been_requested
+    end
+
+    it 'responds with the status and parsed JSON body' do
+      stub_request(:get, /.*/).to_return({
+        status: 200,
+        body: '{"name":"francesco"}',
+        headers: { 'Content-Type' => 'application/json' }
+      })
+
+      response = subject.get('recipients')
+      expect(response).to eq({ status: 200, body: { 'name' => 'francesco' } })
+    end
+  end
+
   describe '#post' do
     it 'sends a POST request to the API server' do
       request = stub_request(:post, 'https://coolpay.herokuapp.com/api/login')
@@ -7,19 +39,26 @@ describe Coolpay::RawClient do
     end
 
     it 'sets the Content-Type header to JSON' do
-      request = stub_request(:any, /.*/).with(headers: { 'Content-Type' => 'application/json' })
+      request = stub_request(:post, /.*/).with(headers: { 'Content-Type' => 'application/json' })
       subject.post('login')
       expect(request).to have_been_requested
     end
 
     it 'encodes the params as JSON' do
-      request = stub_request(:any, /.*/).with(body: '{"username":"francesco"}')
+      request = stub_request(:post, /.*/).with(body: '{"username":"francesco"}')
       subject.post('login', { username: 'francesco' })
       expect(request).to have_been_requested
     end
 
+    it 'uses the optional token as an "Authorization Bearer" token' do
+      request = stub_request(:post, 'https://coolpay.herokuapp.com/api/recipients')
+                  .with(headers: { 'Authorization' => 'Bearer 123-456-789' })
+      subject.post('recipients', {}, '123-456-789')
+      expect(request).to have_been_requested
+    end
+
     it 'responds with the status and parsed JSON body' do
-      stub_request(:any, /.*/).to_return({
+      stub_request(:post, /.*/).to_return({
         status: 200,
         body: '{"token":"0123456789abcdef"}',
         headers: { 'Content-Type' => 'application/json' }
